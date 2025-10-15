7c3ccb9f61c9f2d2f6f7414437b13b10e44354a9
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-15 09:21:03 -0500

    Position Function Added

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 item=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
 echo "${item[@]}"
+read -p "Please enter position: " pos
+echo "Item at $pos is: ${item[$pos]}"
