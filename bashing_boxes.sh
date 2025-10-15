a4d4f95b1cad674a12f602d1d0c3dded39473218
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-15 09:34:22 -0500

    Add Item Function Created Also prints full list with new item

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 item=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
 echo "${item[@]}"
+
+
+
 read -p "Please enter position: " pos
 echo "Item at $pos is: ${item[$pos]}"
+
+
+
+read -p "Please choose a word to add to the list: " word
+ item+=("$word")
+echo "${item[@]}"
+echo "Item '$word' added to list"
