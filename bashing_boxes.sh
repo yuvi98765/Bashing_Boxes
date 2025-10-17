d2831a6c844cf155927432dc62a3ebd01282f825
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-17 09:13:33 -0500

    Made Variable More Clear Than Before

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-item=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
-echo "${item[@]}"
+random_array_of_random_array_of_items=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
+echo "${random_array_of_items[@]}"
+
+random_array_of_random_array_of_items+=("$user_word")
+
+read -p "Please enter positon: " positon_of_word
+echo "Item at $positon_of_word is: ${random_array_of_items[$positon_of_word]}"
 
 
 
-read -p "Please enter position: " position
-echo "Item at $position is: ${item[$position]}"
-
-
-
-read -p "Please choose a word to add to the list: " UserWord
- item+=("$UserWord")
-echo "${item[@]}"
-echo "Item '$UserWord' added to list"
+read -p "Please choose a word to add to the list: " user_word
+ random_array_of_items+=("$user_word")
+echo "${random_array_of_items[@]}"
+echo "your word '$user_word' has been added to the list"
