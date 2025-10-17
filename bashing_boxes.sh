4aca12b9ca3bcd1d5d48b79328241e4c5f8784a0
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-17 09:20:10 -0500

    fixed typos

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-random_array_of_random_array_of_items=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
+random_array_of_items=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
 echo "${random_array_of_items[@]}"
 
-random_array_of_random_array_of_items+=("$user_word")
+random_array_of_items+=("$user_word")
 
-read -p "Please enter positon: " positon_of_word
-echo "Item at $positon_of_word is: ${random_array_of_items[$positon_of_word]}"
+read -p "Please enter position: " position_of_word
+echo "Item at $position_of_word is: ${random_array_of_items[$position_of_word]}"
 
 
 
