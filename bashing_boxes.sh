34e6bd505e5367ae2a4a006ff692cab69b4eb5ef
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-20 09:14:11 -0500

    Interface added

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
-
-while true; do
 random_array_of_items=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
+   
+while true; do
+	echo "1. View all items"
+    echo "2. View an item by position"
+    echo "3. Add a new word"
+    echo "4. Delete the last word"
+    echo "5. Delete a word by position"
+    echo "6. Exit"
+    read -p "Choose an option (1-6): " function_choice
+    echo
+
+   case  $function_choice in
+
+
+
+
+
+1)
+
 echo "${random_array_of_items[@]}"
+;;
 
 
 
+2)
 read -p "Please enter position: " position_of_word
 echo "Item at $position_of_word is: ${random_array_of_items[$position_of_word]}"
+;;
 
 
-
+3)
 read -p "Please choose a word to add to the list: " user_word
  random_array_of_items+=("$user_word")
 echo "${random_array_of_items[@]}"
 echo "your word '$user_word' has been added to the list"
+;;
 
-read -p "you wanna delete the last word in the list  y/n: " function_four_yes_no
-if [[ $function_four_yes_no = n ]]; then
-	
-echo "Okay bye"
-exit 0
-
-else  unset 'random_array_of_items[-1]'
+4)
+ unset 'random_array_of_items[-1]'
 	echo "final item removed"
-fi
+
 echo "${random_array_of_items[@]}"
+;;
 
-
+5)
 read -p "choose The position of the word you want to delete: " user_word_delete
 
 unset "random_array_of_items[$user_word_delete]"
 echo "${random_array_of_items[@]}"
+;;
 
-
-read -p "Would you like to exit the script y/n : " exit_y_n
-if [[ $exit_y_n = y ]]; then
+6)
 	exit 0
-fi
+
+
+;;
+esac
 done
 
