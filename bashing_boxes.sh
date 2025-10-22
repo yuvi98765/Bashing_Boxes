88d1f828554bb4db15719536b8c8f454d70fda35
yuvi98765 <ykarwal@smail.pembinatrails.ca>
2025-10-22 09:19:35 -0500

    Put every line in a method and organized got rid of while true do loop replaced with recursional line used in case function

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,61 +1,79 @@
 #!/bin/bash
+# 1) delaire all needed variables
+# 2) define all functions
+# 3) start main logic
+
 random_array_of_items=("Peanut butter" "Shawl" "Cargo ship" "Wallet" "Xylophone" "Medicine cabinet" "Apron" "Washing machine" "Tree stump" "Milkshake")
    
-while true; do
-	echo "1. View all items"
-    echo "2. View an item by position"
-    echo "3. Add a new word"
-    echo "4. Delete the last word"
-    echo "5. Delete a word by position"
-    echo "6. Exit"
+display_menu_options(){
+	echo " 
+	1. View all items
+	2. View an item by position
+	3. Add a new word
+	4. Delete the last word
+	5. Delete a word by position
+	6. Exit"
     read -p "Choose an option (1-6): " function_choice
-    echo
-
-   case  $function_choice in
-
+    check_user_iput
+}
 
 
 
-
-1)
-
-echo "${random_array_of_items[@]}"
-;;
-
-
-
-2)
-read -p "Please enter position: " position_of_word
-echo "Item at $position_of_word is: ${random_array_of_items[$position_of_word]}"
-;;
-
-
-3)
-read -p "Please choose a word to add to the list: " user_word
- random_array_of_items+=("$user_word")
-echo "${random_array_of_items[@]}"
-echo "your word '$user_word' has been added to the list"
-;;
-
-4)
- unset 'random_array_of_items[-1]'
-	echo "final item removed"
-
-echo "${random_array_of_items[@]}"
-;;
-
-5)
-read -p "choose The position of the word you want to delete: " user_word_delete
-
-unset "random_array_of_items[$user_word_delete]"
-echo "${random_array_of_items[@]}"
-;;
-
-6)
-	exit 0
-
-
-;;
-esac
-done
-
+check_user_iput(){
+	case  $function_choice in
+		1|one)	
+			print_all_items
+			;;
+		2)
+			print_item_at_x_position
+			;;
+		3)
+			print_new_item
+			;;
+		4)
+		 	unset_last_item
+			;;
+		5)
+			delete_word_by_position
+			;;
+		6|exit)
+			exit_script
+			;;
+		*)
+			display_menu_options
+			;;
+	esac
+}
+#done
+print_all_items(){
+			echo "${random_array_of_items[@]}"
+			display_menu_options
+}
+print_item_at_x_position(){
+	read -p "Please enter position 0-9 : " position_of_word
+			echo "Item at $position_of_word is: ${random_array_of_items[$position_of_word]}"
+			display_menu_options
+}
+print_new_item(){
+	read -p "Please choose a word to add to the list: " user_word
+			 random_array_of_items+=("$user_word")
+			echo "${random_array_of_items[@]}"
+			echo "your word '$user_word' has been added to the list"
+			display_menu_options
+}
+unset_last_item(){
+	unset 'random_array_of_items[-1]'
+			echo "final item removed"
+			echo "${random_array_of_items[@]}"
+			display_menu_options
+}
+delete_word_by_position(){
+	read -p "choose The position of the word you want to delete: " user_word_delete
+			unset "random_array_of_items[$user_word_delete]"
+			echo "${random_array_of_items[@]}"
+			display_menu_options
+}
+exit_script(){
+			exit 0
+}
+display_menu_options
